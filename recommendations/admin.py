0d44afb65c5afadf53fc80f3b6aed4c1b9123473
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2025-09-11 23:47:44 -0500

    feat(recommendations): create model and templates for wishlist and recommendations
    implements a wishlist with a modern UI, integration with recommendations, and visual improvements.

--- recommendations/admin.py
+++ recommendations/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Wishlist, ProductRecommendation
 
-# Register your models here.
+admin.site.register(Wishlist)
+admin.site.register(ProductRecommendation)
