87dc324a8394963015d542bd35db09e4eefaae8e
jpjimenezq <jpjimenezq0206@gmail.com>
2025-11-06 23:10:14 -0500

    feat(Advertising-section): advertising display from group 2

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import AboutView, weather_api
+from .views import AboutView, weather_api, ads_api, translations_api
 
 app_name = 'core'
 
 urlpatterns = [
     path('about/', AboutView.as_view(), name='about'),
     path('api/weather/', weather_api, name='weather_api'),
+    path('api/ads/', ads_api, name='ads_api'),
+    path('api/translations/', translations_api, name='translations_api'),
 ]
